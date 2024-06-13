4d3c860edf47cb1152528d0279bf3e8cbaf993ac
Filming <79931565+filming@users.noreply.github.com>
2024-06-13 15:24:20 -0400

    feat: create sign-up form in auth BP

---
+++ dashlink/src/auth/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
+
+class SignupForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Sign Up")
