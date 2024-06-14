70910faabe7fef17cedec6e4af7c671cab87f440
Filming <79931565+filming@users.noreply.github.com>
2024-06-13 22:15:51 -0400

    fix: change repeated password label text

--- dashlink/src/auth/forms.py
+++ dashlink/src/auth/forms.py
@@ -6,5 +6,5 @@ class SignupForm(FlaskForm):
     email = EmailField("Email", validators=[DataRequired()])
     username = StringField("Username", validators=[DataRequired(), Length(min=3, max=16)])
     password = PasswordField("Password", validators=[DataRequired()])
-    password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
+    password2 = PasswordField("Password (Confirm)", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Sign Up")
