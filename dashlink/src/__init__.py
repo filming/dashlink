ac3f0373374744777b7c97892215a095d02fb124
Filming <79931565+filming@users.noreply.github.com>
2024-06-13 22:34:24 -0400

    feat: intializing db obj within app factory

--- dashlink/src/__init__.py
+++ dashlink/src/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 
 from config import Config
+from src.extensions import db
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     # initializing flask extensions
-    
+    db.init_app(app)
 
     # registering blueprints
     from src.main import bp as main_bp
