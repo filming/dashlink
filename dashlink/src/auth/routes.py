d72ca19ae7117cb3d4c036a91b2e65e3816546b7
Filming <79931565+filming@users.noreply.github.com>
2024-06-15 02:18:51 -0400

    feat: validate user creds from login form

--- dashlink/src/auth/routes.py
+++ dashlink/src/auth/routes.py
@@ -4,12 +4,18 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from src.auth import bp
 from src.auth.forms import SignupForm, LoginForm
+from src.models.user import User
+from src.extensions import db
 
 @bp.route("/login", methods = ["GET", "POST"])
 def login():
     form = LoginForm()
 
     if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+
+        if user and check_password_hash(user.password, form.password.data):
+            return redirect(url_for('main.index'))
 
         return redirect(url_for('auth.login'))
 
