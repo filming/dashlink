2415554b95b4201b08b27ffbf71782ffedca8fab
Filming <79931565+filming@users.noreply.github.com>
2024-06-15 01:20:13 -0400

    feat: add custom validators for sign-up form

--- dashlink/src/auth/forms.py
+++ dashlink/src/auth/forms.py
@@ -1,6 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.validators import DataRequired, EqualTo, Length, ValidationError
+
+from src.models.user import User
+
+def unique_email(form, field):
+    user = User.query.filter_by(email = field.data).first()
+
+    if user:
+        raise ValidationError("Email address is already registered.")
+    
+def unique_username(form, field):
+    user = User.query.filter_by(username = field.data).first()
+
+    if user:
+        raise ValidationError("Username is taken.")
 
 class SignupForm(FlaskForm):
     email = EmailField("Email", validators=[DataRequired()])
