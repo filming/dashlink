375d62ca82b3d76a7860f9ab0551243258287cd6
Filming <79931565+filming@users.noreply.github.com>
2024-06-15 02:24:52 -0400

    feat: import models and create a database

--- dashlink/src/__init__.py
+++ dashlink/src/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+from os import path
+
 from config import Config
 from src.extensions import db
 
@@ -17,4 +19,15 @@ def create_app(config_class=Config):
     from src.auth import bp as auth_bp
     app.register_blueprint(auth_bp)
 
+    # setting up database
+    from src.models.user import User
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists(f"src/dashlink.db"):
+        with app.app_context():
+            db.create_all()
+            print ("Created database!")
