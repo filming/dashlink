e564421cf7954e14c3dc4cfd4baaa4af682c10f4
Filming <79931565+filming@users.noreply.github.com>
2024-06-18 05:10:51 -0400

    feat: home page is only viewable if logged in

--- dashlink/src/main/routes.py
+++ dashlink/src/main/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template
+from flask_login import login_required, current_user
+
 from src.main import bp
 
 @bp.route("/")
+@login_required
 def index():
     return render_template("index.html")
