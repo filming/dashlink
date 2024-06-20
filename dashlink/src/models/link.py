95bdb0c4ebc60a12a56a76189a1e043fb7de59f0
Filming <79931565+filming@users.noreply.github.com>
2024-06-20 03:11:05 -0400

    feat: create link model

---
+++ dashlink/src/models/link.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+from src.extensions import db
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    short_link = db.Column(db.String(32), index = True, unique = True)
+    original_link = db.Column(db.String(4096), index = True, unique = False)
+    created_at = db.Column(db.DateTime(), index = True, unique = False, default = datetime.now())
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
