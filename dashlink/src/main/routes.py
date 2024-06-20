25ebdf8a105d2c0ef13920840e5278704da429dc
Filming <79931565+filming@users.noreply.github.com>
2024-06-20 03:09:53 -0400

    feat: query all links to send them to home template

--- dashlink/src/main/routes.py
+++ dashlink/src/main/routes.py
@@ -2,8 +2,11 @@ from flask import render_template
 from flask_login import login_required, current_user
 
 from src.main import bp
+from src.models.link import Link
 
 @bp.route("/")
 @login_required
 def index():
-    return render_template("index.html", user=current_user)
+    links = Link.query.all()
+
+    return render_template("index.html", user=current_user, links=links)
