2a80daf57d99daa01c2d77394045a2f449546dcf
Filming <79931565+filming@users.noreply.github.com>
2024-06-21 09:57:48 -0400

    fix: remove unnecessary func

--- dashlink/src/main/forms.py
+++ dashlink/src/main/forms.py
@@ -2,9 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired, URL
 
-def valid_url(form, field):
-    clean_data = form.data.strip()
-
 class AddURL(FlaskForm):
     link = TextAreaField("Add URL", render_kw={"rows": 3, "cols": 50, "placeholder":"Enter URL"}, validators=[DataRequired(), URL()])
     submit = SubmitField("Add")
