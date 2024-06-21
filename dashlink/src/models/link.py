98d550cda79ecc38beaa877b4d5f772b797c6405
Filming <79931565+filming@users.noreply.github.com>
2024-06-21 06:48:24 -0400

    feat: add default value to uses column

--- dashlink/src/models/link.py
+++ dashlink/src/models/link.py
@@ -7,6 +7,6 @@ class Link(db.Model):
     short_code = db.Column(db.String(8), index = True, unique = True)
     short_link = db.Column(db.String(32), index = True, unique = True)
     original_link = db.Column(db.String(4096), index = True, unique = False)
-    uses = db.Column(db.Integer, index = True, unique = False)
+    uses = db.Column(db.Integer, index = True, unique = False, default = 0)
     created_at = db.Column(db.DateTime(), index = True, unique = False, default = datetime.now())
     creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
