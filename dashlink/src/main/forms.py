35655cac2b4510fb06cf92701330aba7ae091a4d
Filming <79931565+filming@users.noreply.github.com>
2024-06-21 09:54:22 -0400

    feat: add url validator to addURL form class

--- dashlink/src/main/forms.py
+++ dashlink/src/main/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, URL
+
+def valid_url(form, field):
+    clean_data = form.data.strip()
 
 class AddURL(FlaskForm):
-    link = TextAreaField("Add URL", render_kw={"rows": 3, "cols": 50, "placeholder":"Enter URL"}, validators=[DataRequired()])
+    link = TextAreaField("Add URL", render_kw={"rows": 3, "cols": 50, "placeholder":"Enter URL"}, validators=[DataRequired(), URL()])
     submit = SubmitField("Add")
