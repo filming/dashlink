cddeb340b08700a677a82a1af101b91396069b34
Filming <79931565+filming@users.noreply.github.com>
2024-06-21 05:06:02 -0400

    feat: create func to generate short code

--- dashlink/src/main/routes.py
+++ dashlink/src/main/routes.py
@@ -1,8 +1,21 @@
 from flask import render_template
 from flask_login import login_required, current_user
 
+from random import choices
+from string import ascii_lowercase, digits
+
+from src.extensions import db
 from src.main import bp
 from src.models.link import Link
+from src.main.forms import AddURL
+
+BASE_URL = "dashlink.net"
+AVAILABLE_CHARS = ascii_lowercase + digits
+
+def generate_short_code():
+    short_code = "".join(choices(AVAILABLE_CHARS, k=7))
+    
+    return "".join(short_code)
 
 @bp.route("/")
 @login_required
